7976e07a94b5f144eaeaba04a672878c4aa046a9
Jordy Verbeek <verbeek.jordy@gmail.com>
2020-02-25 11:44:47 +0100

    Added layout to the cart and added buttons to remove items in the cart.

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Gift} from '../../models/gift.model';
 import {GiftService} from '../../services/gift.service';
 
@@ -9,11 +9,21 @@ import {GiftService} from '../../services/gift.service';
 })
 export class CartComponent implements OnInit {
   gifts: Array<Gift>;
+  count: number;
 
-  constructor(private service: GiftService) { }
+  constructor(private service: GiftService) {
+  }
 
   ngOnInit() {
     this.service.getGifts('').subscribe(data => this.gifts = data);
+  }
+
+  removeItemFromCart(gift: Gift) {
+    this.count = 0;
+    while (gift.id !== this.gifts[this.count].id) {
+      this.count++;
+    }
+    this.gifts.splice(this.count, 1);
   }
 
 }
