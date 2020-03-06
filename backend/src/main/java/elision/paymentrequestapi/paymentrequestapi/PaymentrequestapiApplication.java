ec62c299b9561e353279430cd911d36d81abac5e
Jordy Verbeek <verbeek.jordy@gmail.com>
2020-03-06 08:50:03 +0100

    Added the initial java project

---
+++ backend/src/main/java/elision/paymentrequestapi/paymentrequestapi/PaymentrequestapiApplication.java
@@ -0,0 +1,13 @@
+package elision.paymentrequestapi.paymentrequestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentrequestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentrequestapiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/elision/paymentrequestapi/paymentrequestapi/PaymentrequestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package elision.paymentrequestapi.paymentrequestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentrequestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
